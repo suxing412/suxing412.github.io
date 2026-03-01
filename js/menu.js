513180f425ff9d4e242d65d4e2bd164f09a9948c
Xing Su <143617128+suxing412@users.noreply.github.com>
2026-03-01 18:24:30 -0500

    update

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    const navList = document.getElementById('nav-list');
-    if (navList.style.display === 'flex') {
-        navList.style.display = 'none';
-    } else {
-        navList.style.display = 'flex';
-    }
-});
